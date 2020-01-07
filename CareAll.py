2249e3ed7a7979b74dbe81d01575ff10c4f1a13e
pavanpravesh <59290013+pavanpravesh@users.noreply.github.com>
2020-01-07 16:23:27 +0530

    Add files via upload

---
+++ CareAll.py
@@ -0,0 +1,29 @@
+class old():
+    n=int()
+    def __init__(self, ratig, review, pay):
+        self.ratig= 'ratig'
+        self.review= 'review'
+        self.pay= 'pay'
+
+        if n<=4 & self.rating==True & self.review==True :
+            print(self.pay("True"))
+        else:
+            return 0
+class Youth(old):
+    n=int()
+    def __init__(self, ratig, review, pay):
+        self.ratig= 'ratig'
+        self.review= 'review'
+        self.pay= 'pay'
+
+        if n<=4 & self.rating==True & self.review==True :
+            print(self.pay('True'))
+        else:
+            return 0
+
+
+
+
+
+
+
